820bc94334a92363fa5fe7f747c06bc85af2438d
aruntakkar <aruntakkar5@gmail.com>
2015-11-24 00:42:58 +0530

    Angular Factories Example

---
+++ scripts/AngularFactories.js
@@ -0,0 +1,13 @@
+var demoApp = angular.module('demoApp', [])
+    .factory('simpleFactory', function() {
+        var factory = {};
+        var customers = [...];
+        factory.getCustomers = function() {
+            return customers;
+        };
+        return factory;
+    })
+    .controller('simpleController', function($scope,simpleFactory) {
+    	// Factory injected into controller at runtime
+
+    })
