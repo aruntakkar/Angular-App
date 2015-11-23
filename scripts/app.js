eb19e9c5bd249b7c0701b47581d6fa0c98d51f30
aruntakkar <aruntakkar5@gmail.com>
2015-11-23 21:20:21 +0530

    javascript in app.js

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+// Defining Module
+var demoApp = angular.module('demoApp', ['ngRoute']);
+
+// Defining Module Routes
+demoApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/view1', {
+            controller: 'simpleController',
+            templateUrl: 'partials/view1.html'
+        })
+        .when('/view2', {
+            controller: 'simpleController',
+            templateUrl: 'partials/view2.html'
+        })
+        .otherwise({
+            redirectTo: '/view1'
+        });
+});
+
+// Defining Controller
+demoApp.controller('simpleController', function($scope) {
+    $scope.customers = [{
+        name: 'Dave Jones',
+        city: 'Phoenix'
+    }, {
+        name: 'Jamie Riley',
+        city: 'Atlanta'
+    }, {
+        name: 'Heedy Wahlin',
+        city: 'Chandler'
+    }, {
+        name: 'Thomas Wahlin',
+        city: 'Seattle'
+    }];
+
+});
+
+// Defining addCustomer for Adding New Customers
+$scope.addCustomer = function() {
+    $scope.customer.push({
+        name: $scope.newCustomer.name,
+        city: $scope.newCustomer.city
+    });
+};
